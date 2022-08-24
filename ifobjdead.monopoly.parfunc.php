82f061bf829ad4c65e2b293db96554447d80638c
Karl August Nestor <jesuscaesar@outlook.com>
2022-08-25 04:07:48 +1000

    Added new files

--- ifobjdead.monopoly.parfunc.php
+++ ifobjdead.monopoly.parfunc.php
@@ -7,5 +7,5 @@ $subAction = $subActions[rand(0, $subActionCount - 1)];
 if ($subAction == "pass") {
     $subRating += 0.01;
     $subScore += 1;
-    echo $turnNum." : ".$subHalfNotation.' '.$spacedictus[$proLingo]["pass"]."<br>";
+    echo $turnNum." : ".$subFullName.' '.$spacedictus[$proLingo]["pass"]."<br>";
 }
