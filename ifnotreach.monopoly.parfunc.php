d9f5275bef2318e83c97b0c667811fc6f1c392c7
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-06 01:00:01 +0000

    Latest update

--- ifnotreach.monopoly.parfunc.php
+++ ifnotreach.monopoly.parfunc.php
@@ -5,11 +5,11 @@ $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "vendor") {
-    $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem, $zoneArr);
+    $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem);
     $proMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 } elseif ($subAction == "withdraw") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem, $zoneArr);
+    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem);
     $subMoney = $msgBox['debit'];
     $proMoney = $msgBox['credit'];
 }
--- onrl.monopoly.parfunc.php
+++ onrl.monopoly.parfunc.php
@@ -5,19 +5,19 @@ $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "buy") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseItem, $zoneArr);
+    $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseItem);
     $objMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 } elseif ($subAction == "sell") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subUseItem, $zoneArr);
+    $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subUseItem);
     $subMoney = $msgBox['debit'];
     $objMoney = $msgBox['credit'];
 } elseif ($subAction == "vendor") {
-    $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem, $zoneArr);
+    $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem);
     $proMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 } elseif ($subAction == "withdraw") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem, $zoneArr);
+    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem);
     $subMoney = $msgBox['debit'];
     $proMoney = $msgBox['credit'];
 }
