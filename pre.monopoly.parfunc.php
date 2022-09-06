995b2d3d7a0545436c7782846be223e74a432edd
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-06 01:59:47 +0000

    Latest update

--- pre.monopoly.parfunc.php
+++ pre.monopoly.parfunc.php
@@ -3,3 +3,15 @@
 $proUseItem = shopFor('.', 'item');
 $subUseItem = shopFor($sub, 'item');
 $objUseItem = shopFor($obj, 'item');
+
+$proUseWeapon = shopFor('.', 'weapon');
+$subUseWeapon = shopFor($sub, 'weapon');
+$objUseWeapon = shopFor($obj, 'weapon');
+
+$proUseMelee = shopFor('.', 'melee');
+$subUseMelee = shopFor($sub, 'melee');
+$objUseMelee = shopFor($obj, 'melee');
+
+$proUseShield = shopFor('.', 'shield');
+$subUseShield = shopFor($sub, 'shield');
+$objUseShield = shopFor($obj, 'shield');
