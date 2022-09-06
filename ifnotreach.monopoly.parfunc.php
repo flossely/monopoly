9920a8fe059d4286ef4779638262fd6cb962b7f8
Karl August Nestor <jesuscaesar@outlook.com>
2022-09-06 01:46:55 +0000

    Latest update

--- ifnotreach.monopoly.parfunc.php
+++ ifnotreach.monopoly.parfunc.php
@@ -5,11 +5,11 @@ $subActionCount = count($subActions);
 $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "vendor") {
-    $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem);
+    $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem, $zoneArr);
     $proMoney = $msgBox['debit'];
     $subMoney = $msgBox['credit'];
 } elseif ($subAction == "withdraw") {
-    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem);
+    $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem, $zoneArr);
     $subMoney = $msgBox['debit'];
     $proMoney = $msgBox['credit'];
 }
