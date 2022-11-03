2db357e7c1726c853d72f790a4f8ea6668235f1e
Karl August Nestor <jesuscaesar@outlook.com>
2022-11-03 02:58:57 +0000

    Improved functionality

--- ifself.monopoly.parfunc.php
+++ ifself.monopoly.parfunc.php
@@ -6,10 +6,10 @@ $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "vendor") {
     $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem, $zoneArr);
-    $proMoney = $msgBox['debit'];
-    $subMoney = $msgBox['credit'];
+    $proMoney = $msgBox['left'];
+    $subMoney = $msgBox['right'];
 } elseif ($subAction == "withdraw") {
     $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem, $zoneArr);
-    $subMoney = $msgBox['debit'];
-    $proMoney = $msgBox['credit'];
+    $subMoney = $msgBox['left'];
+    $proMoney = $msgBox['right'];
 }
--- oncc.monopoly.parfunc.php
+++ oncc.monopoly.parfunc.php
@@ -6,18 +6,18 @@ $subAction = $subActions[rand(0, $subActionCount - 1)];
 
 if ($subAction == "buy") {
     $msgBox = initExchange($thisParadigm, $yearToday, $obj, $sub, $objUseItem, $zoneArr);
-    $objMoney = $msgBox['debit'];
-    $subMoney = $msgBox['credit'];
+    $objMoney = $msgBox['left'];
+    $subMoney = $msgBox['right'];
 } elseif ($subAction == "sell") {
     $msgBox = initExchange($thisParadigm, $yearToday, $sub, $obj, $subUseItem, $zoneArr);
-    $subMoney = $msgBox['debit'];
-    $objMoney = $msgBox['credit'];
+    $subMoney = $msgBox['left'];
+    $objMoney = $msgBox['right'];
 } elseif ($subAction == "vendor") {
     $msgBox = initExchange($thisParadigm, $yearToday, '.', $sub, $proUseItem, $zoneArr);
-    $proMoney = $msgBox['debit'];
-    $subMoney = $msgBox['credit'];
+    $proMoney = $msgBox['left'];
+    $subMoney = $msgBox['right'];
 } elseif ($subAction == "withdraw") {
     $msgBox = initExchange($thisParadigm, $yearToday, $sub, '.', $subUseItem, $zoneArr);
-    $subMoney = $msgBox['debit'];
-    $proMoney = $msgBox['credit'];
+    $subMoney = $msgBox['left'];
+    $proMoney = $msgBox['right'];
 }
